3e6b431ae4bb876fca04136d3f2183f5225fa1e4
enrico <rombaldonienrico@gmail.com>
2021-01-22 16:36:47 +0100

    scaffolding

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+
+<?php
+  
+ ?>
+ <header>
+   <div class="wrapper_margin">
+    <div id="header_logo">
+        <a href="#">
+        <img src="" alt="">
+        </a>
+    </div>
+   </div>
+ </header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Stampare a schermo una decina di dischi musicali
+(vedi screenshot) in due modi diversi:
+- Solo con l’utilizzo di PHP, che stampa
+direttamente i dischi in pagina: al caricamento
+della pagina ci saranno tutti i dischi. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>php-ajax-dischi</title>
+</head>
+<body>
+    <?php 
+    include __DIR__."/includes/header.php";
+    include __DIR__."/includes/main.php";
+    include  __DIR__."/includes/footer.php";
+    ?>   
+</body>
+</html>
