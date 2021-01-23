3eaa0f129c0adabd6b8f22159efb4f6aaebece4b
enrico <rombaldonienrico@gmail.com>
2021-01-23 14:18:02 +0100

    fix card

--- includes/main.php
+++ includes/main.php
@@ -7,14 +7,12 @@
         foreach ( $libraryDisk["response"] as $data ) {
         ?>
             <div class=box_card_disk>
-            <img src="<?=$data["poster"]?>" alt="<?='Cover_abum'.$data["title"]?>">
-            <h4 class="title_disk"><?=$data["title"]?></h4>
-            <p class="author"><?=$data["author"]?></p>
-            <p class="year"><?=$data["year"]?></p>
-
-
-            
-            
+                <div class="card">
+                    <img src="<?=$data["poster"]?>" alt="<?='Cover_abum'.$data["title"]?>">
+                    <h4 class="title_disk"><?=$data["title"]?></h4>
+                    <p class="author"><?=$data["author"]?></p>
+                    <p class="year"><?=$data["year"]?></p>
+                </div>
             </div>
         
             <?php
